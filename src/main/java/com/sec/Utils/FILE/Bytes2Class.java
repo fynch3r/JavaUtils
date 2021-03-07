ec5ff62484117f00095dde6c42b14a7797c2c275
0range228 <infosec_fxc@163.com>
2021-03-07 13:40:58 +0800

    2021/3/7

--- src/main/java/com/sec/Utils/FILE/Bytes2Class.java
+++ src/main/java/com/sec/Utils/FILE/Bytes2Class.java
@@ -1,37 +1,1 @@
-package com.sec.Utils.FILE;
-
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
-/**
- * @program: Utils
- * @description:
- * @author: 0range
- * @create: 2021-03-05 16:31
- **/
-
-
-public class Bytes2Class {
-    //byte[] to .class
-    public static void byte2class(byte[] bytes, String filepath)  {
-        File file = new File(filepath);
-
-        try {
-            if(!file.exists() && file.isDirectory()){
-                file.mkdirs();
-            }
-
-            FileOutputStream fos = new FileOutputStream(file);
-            BufferedOutputStream bos = new BufferedOutputStream(fos);
-
-            bos.write(bytes);
-            bos.close();
-            fos.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-}
+package com.sec.Utils.FILE;import java.io.BufferedOutputStream;import java.io.File;import java.io.FileOutputStream;import java.io.IOException;import java.sql.Time;/** * @program: Utils * @description: * @author: 0range * @create: 2021-03-05 16:31 **/public class Bytes2Class {    //byte[] to .class    public static void byte2class(byte[] bytes, String filepath)  {        File file = new File(filepath);        try {            if(!file.exists() && file.isDirectory()){                file.mkdirs();            }            FileOutputStream fos = new FileOutputStream(file);            BufferedOutputStream bos = new BufferedOutputStream(fos);            bos.write(bytes);            bos.close();            fos.close();        } catch (IOException e) {            e.printStackTrace();        }    }}
